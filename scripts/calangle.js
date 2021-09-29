9099c32e149eca5e844e2a146bd99abf8bc62030
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-30 02:57:26 +0530

    3 pages working

--- scripts/calangle.js
+++ scripts/calangle.js
@@ -7,26 +7,29 @@ var out=document.querySelector("#mesg");
 button.addEventListener("click",takeInput)
 
 function takeInput(){
-    var side1=s1.value;
-    if(side1){
-        var side2 =s2.value;
+    var side1=Number(s1.value);
+    if(side1 && side1>0){
+        var side2 =Number(s2.value && side2>0);
         if(side2){
-            var side3=s3.value;
+            var side3=Number(s3.value && side3>0);
             if(side3){
                 calculate(side1,side2,side3)
             }
             else{
                 console.log("enter side 3")
+                showMessage("enter side 3 properly")
             }
 
         }
         else{
             console.log("enter side2")
+            showMessage("enter side 2 properly")
         }
 
     }
     else{
         console.log("enter side 1")
+        showMessage("enter side 1 properly")
 
     }
 
@@ -35,6 +38,7 @@ function takeInput(){
 
 function calculate(a,b,c){
     var h=a+b+c
+    console.log(h)
     if(h===180){
        var   an="it is a triangle"
        showMessage(an)
@@ -53,3 +57,4 @@ function showMessage(a){
    out.innerText=a;
     
 }
+
