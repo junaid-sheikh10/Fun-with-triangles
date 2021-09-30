0671cd3805d9966fa18ccd3de55ae3284ee1edfe
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-30 14:07:50 +0530

    quiz page added

---
+++ scripts/quiz.js
@@ -0,0 +1,40 @@
+const quizform = document.querySelector(".quiz-form");
+const submit = document.querySelector("#btn");
+const outputEl = document.querySelector(".outputdiv");
+
+const correctAnswers = [
+    "90°",
+    "one right angle",
+    "12, 16, 20",
+    "Equilateral triangle",
+    "100°",
+    "30°",
+    "a + b + c",
+    "no",
+    "45°",
+  ];
+
+
+
+
+function calculateScore() {
+    const formResults = new FormData(quizform);
+    let score = 0,
+      index = 0;
+    for (let value of formResults.values()) {
+      if (value === correctAnswers[index]) {
+        score = score + 1;
+      }
+      index = index + 1;
+    }
+     console.log("The score is "+score);
+    outputEl.innerText = "The score is " + score;
+  }
+  
+  submit.addEventListener("click", clickhandler);
+
+  
+  
+  function clickhandler(){
+      console.log("clicked");
+  }
